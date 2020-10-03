db1d8a3619c295ec591780f3f465e2f2e47de25b
eggdropsoap <eggdropsoap@pipemaze.com>
2020-10-03 16:47:40 -0700

    Directly fix too-narrow columns in 10.4.1.7
    Mostly affects context output.
    Fix too-narrow columns directly by reducing width of
    largest column.
    Removing --columns argument in create_context script as that
    kludge is longer needed.

--- create_context.sh
+++ create_context.sh
@@ -1,3 +1,4 @@
 echo Creating ConTeXt...
-pandoc markdown/QuestWorlds.md -s -t context --toc --top-level-division=chapter --template=context.template -o docs/context/QuestWorlds.tex -V mainfont:'ebgaramond' -V fontsize:'11pt' --lua-filter filters/stripHeaderNumbers.lua --columns=10000
+pandoc markdown/QuestWorlds.md -s -t context --toc --top-level-division=chapter --template=context.template -o docs/context/QuestWorlds.tex -V mainfont:'ebgaramond' -V fontsize:'11pt' --lua-filter filters/stripHeaderNumbers.lua
+#--columns=10000
 
