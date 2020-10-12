ae831c60503d55040a1e2605dbfa33181860243f
eggdropsoap <eggdropsoap@pipemaze.com>
2020-10-12 11:49:15 -0700

    Fix create script for context book

--- create_context.sh
+++ create_context.sh
@@ -3,6 +3,6 @@ echo -n "Creating ConTeXt SRD... "
 pandoc markdown/QuestWorlds.md -s -t context --toc --top-level-division=chapter --template=context.template -o docs/context/QuestWorlds.tex -V mainfont:'ebgaramond' -V fontsize:'11pt' --lua-filter filters/stripHeaderNumbers.lua
 echo done.
 echo -n "Creating ConTeXt book... "
-pandoc markdown/QuestWorlds.md -s -t context --toc --top-level-division=chapter --template=context.template -o docs/context/QuestWorlds.tex -V mainfont:'ebgaramond' -V fontsize:'11pt' --lua-filter filters/stripHeaderNumbers.lua
+pandoc markdown/QuestWorlds.md -s -t context --toc --top-level-division=chapter --template=context.book.template -o docs/context/QuestWorlds.book.tex -V mainfont:'ebgaramond' -V fontsize:'12pt' --lua-filter filters/stripHeaderNumbers.lua
 echo done.
 
